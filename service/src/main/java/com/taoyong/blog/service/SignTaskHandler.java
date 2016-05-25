a5320dac34e5d2c77266ef91c1eb41c3ef29bbb4
taoyong <taoyong0728@126.com>
2016-05-25 17:47:56 +0800

    签到定时任务

---
+++ service/src/main/java/com/taoyong/blog/service/SignTaskHandler.java
@@ -0,0 +1,12 @@
+package com.taoyong.blog.service;
+
+/**
+ * Created by taoyong1 on 2016/5/25.
+ */
+public interface SignTaskHandler {
+
+    /**
+     * 定时签到任务
+     */
+    void signIn();
+}
---
+++ service/src/main/java/com/taoyong/blog/service/impl/SignTaskHandlerImpl.java
@@ -0,0 +1,12 @@
+package com.taoyong.blog.service.impl;
+
+import com.taoyong.blog.service.SignTaskHandler;
+
+/**
+ * Created by taoyong1 on 2016/5/25.
+ */
+public class SignTaskHandlerImpl implements SignTaskHandler {
+    public void signIn() {
+
+    }
+}
